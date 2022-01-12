579f44edc3392b12aa2cae471b42e363ee8334ac
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2022-01-12 17:40:32 -0500

    Version 0.0.6

--- dicom2hdf/__init__.py
+++ dicom2hdf/__init__.py
@@ -4,7 +4,7 @@ from .logging_tools import logs_file_setup
 from .paths_manager.path_generator import PathGenerator
 
 __author__ = "Maxence Larose"
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __copyright__ = "Copyright 2022, Maxence Larose"
 __credits__ = ["Maxence Larose"]
 __license__ = "GPL"
