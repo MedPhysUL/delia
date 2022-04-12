2b700d461e2a082b4da213b0ace50fb739205319
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2022-04-12 11:25:48 -0400

    Refractoring examples

--- examples/ex01.py
+++ examples/ex01.py
@@ -1,33 +1,31 @@
 """
-    @Title:             Logging + dataset creation.
+    @Title:             On-the-fly tasks.
 
-    @Description:       Configure logging and create dataset.
+    @Description:       Perform on-the-fly tasks on images.
 """
 
 import env_examples  # Modifies path, DO NOT REMOVE
 
-from dicom2hdf import PatientsDataset
+from dicom2hdf import PatientsDataGenerator
+import SimpleITK as sitk
 
 
 if __name__ == "__main__":
     # ----------------------------------------------------------------------------------------------------------- #
-    #                                         Logs Setup (Optional)                                               #
+    #                                    Perform on-thy-fly tasks on images                                       #
     # ----------------------------------------------------------------------------------------------------------- #
-    env_examples.configure_logging("logging_conf.yaml")
-
-    # ----------------------------------------------------------------------------------------------------------- #
-    #            Create dataset (some images of some patients might fail to be added to the dataset)              #
-    # ----------------------------------------------------------------------------------------------------------- #
-    dataset = PatientsDataset(
-        path_to_dataset="data/patients_dataset.h5",
-    )
-
-    patients_who_failed = dataset.create_hdf5_dataset(
+    patients_data_generator = PatientsDataGenerator(
         path_to_patients_folder="data/Patients",
-        tags_to_use_as_attributes=[(0x0008, 0x103E), (0x0020, 0x000E), (0x0008, 0x0060)],
-        series_descriptions="data/series_descriptions.json",
-        overwrite_dataset=True
+        series_descriptions="data/series_descriptions.json"
     )
 
-    # Print list of patients who failed
-    print(f"Patients who failed the pipeline : {patients_who_failed}")
+    for patient_data in patients_data_generator:
+        print(f"Patient ID: {patient_data.patient_id}")
+
+        for patient_image_data in patient_data.data:
+            dicom_header = patient_image_data.image.dicom_header
+            simple_itk_image = patient_image_data.image.simple_itk_image
+            numpy_array_image = sitk.GetArrayFromImage(simple_itk_image)
+
+            """Perform any tasks on images on-the-fly."""
+            print(numpy_array_image.shape)
