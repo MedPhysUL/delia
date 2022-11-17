bb9754298d1544288542a042779eab7d92391e00
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2022-11-17 14:53:48 -0500

    Bumps version to 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="delia",
-    version="1.0.5",
+    version="1.0.6",
     author="Maxence Larose",
     author_email="maxence.larose.1@ulaval.ca",
     description="DICOM Extraction for Large-scale Image Analysis (DELIA).",
