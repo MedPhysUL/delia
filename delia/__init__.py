abda3ac2566df4c0f1016276d8cca36c6e6805f6
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-12-27 13:57:27 -0500

    Bumps to version 1.2.7

--- delia/__init__.py
+++ delia/__init__.py
@@ -9,7 +9,7 @@ stream_handler.setLevel(logging.WARNING)
 logging.getLogger(__name__).addHandler(stream_handler)
 
 __author__ = "Maxence Larose"
-__version__ = "1.2.6"
+__version__ = "1.2.7"
 __copyright__ = "Copyright 2022, Maxence Larose"
 __credits__ = ["Maxence Larose"]
 __license__ = "Apache License 2.0"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="delia",
-    version="1.2.6",
+    version="1.2.7",
     author="Maxence Larose",
     author_email="maxence.larose.1@ulaval.ca",
     description="DICOM Extraction for Large-scale Image Analysis (DELIA).",
